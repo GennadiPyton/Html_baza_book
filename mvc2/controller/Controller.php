77747384767412d54a781cc681c08102c675ac22
GennadiPyton <73903012+GennadiPyton@users.noreply.github.com>
2021-11-12 18:03:51 +0200

    Update Controller.php
    добавляю коментарий

--- mvc2/controller/Controller.php
+++ mvc2/controller/Controller.php
@@ -1,15 +1,15 @@
 <?php
 
 
-
+// Controller Basics
 class Controller{
 
 
-
+// access without instantiating the class.
     public function StartSite(){
         include 'view/main.php';
     }
-
+// functions can be accessed without creating an object.
     public function BookList(){
         $booksList = Model::getBookList();
         include 'view/bookList.php';
@@ -21,10 +21,12 @@ class Controller{
             $book = $test[1];
             include 'view/bookOne.php';
         }
+        // connecting error checking
         else {
             include 'view/error404.php';
         }
     }
+    // functions don't work (error404).
     public function error404(){
         include 'view/error404.php';
     }
