1552d7e5911e53e34a44af2763e79a5a44b4bbd7
GennadiPyton <73903012+GennadiPyton@users.noreply.github.com>
2021-11-12 18:10:55 +0200

    Update index.php
    добавляю комментарий

--- mvc2/index.php
+++ mvc2/index.php
@@ -1,7 +1,9 @@
 <?php
+// includes and evaluates the specified file during script execution.
 include_once 'model/Model.php';
 include_once 'controller/controller.php';
 include_once 'route/routing.php';
+// Output one or more strings
 echo $response;
 
 
