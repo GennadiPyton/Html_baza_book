9760692c3939c899bb3afff192bc3e8afd1185f3
GennadiPyton <73903012+GennadiPyton@users.noreply.github.com>
2021-11-12 18:05:07 +0200

    Update Model.php
    добавляю коментарий

--- mvc2/model/Model.php
+++ mvc2/model/Model.php
@@ -1,14 +1,14 @@
 <?php
 
 
-
+// received a list of all books.
     class Model{
 
         public function getBookList() {
             require_once 'model/booksArray.php';
             return $books;
         }
-
+// using the get one book function shows that each book has its own name (returns one book or the entire list of books for review).
         public function getBook($title){
             $allBook = Model::getBookList();
             $i = 0;
